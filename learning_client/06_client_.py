b57157b94609738b72603cee49a2cd91e8cbfa21
SyedSafdarAliShah <programmersafdar@live.com>
2025-07-28 08:32:27 +0500

    client updated

---
+++ learning_client/06_client_.py
@@ -0,0 +1,21 @@
+import request 
+URL = "http://localhost:8000/mcp"
+PAYLOAD = {
+    "jsonrpc": "2.0",
+    "method":"mcp",
+    "params": {},
+    "id": 1,
+}
+HEADER = {
+    "Content-Type":"application/json",
+    "Accept": "application/json, text/event-stream"
+}
+response = request.post(URL,json=PAYLOAD,headers=HEADER,stream=True)
+for line in response.iter_lines():
+    if line:
+        print(line.decode('utf-8'))
+        # decode bytes to string and print
+        # for example you could parse json if the response is in json format
+        # import json
+        # data = json.loads(line.decode('utf-8'))
+        # print(data) 
