7488cd812cb34434d5dedcc5f6591f4fbe07a57d
SyedSafdarAliShah <programmersafdar@live.com>
2025-07-31 13:07:07 +0500

    Server Client Working

--- client.py
+++ client.py
@@ -18,23 +18,25 @@ class MCPClient:
         # await self.session.close()
         await self.stack.aclose()
     
-    async def list_tool(self) -> types.Tool:
+    async def list_tool(self) -> list[types.Tool]:
         return (await self._session.list_tools()).tools
     
-    async def call_tool(self,tool_name, *args, **kwargs) -> types.ToolCall:
+    async def call_tool(self,tool_name, *args, **kwargs):
         return await self._session.call_tool(tool_name, *args, **kwargs)
     
-    async def resourse(self):
-        return await self._session.resource()
-    
-    async def main():
-        async with MCPClient("http://localhost:8000") as client:
-            tools = await client.list_tool()
-            print("Available tools:", tools)
-            if tools:
-                tool_name = tools[0].name
-                result = await client.call_tool(tool_name, "example_arg")
-                print(f"Result from {tool_name}:", result)
-            resource = await client.resourse()
-            print("Resource info:", resource)   
-            
+    async def resource(self) -> list[types.Resource]:
+        result = await self._session.list_resources()
+        return result.resources if result else []
+async def main():
+    async with MCPClient("http://localhost:8000/mcp") as client:
+        tools = await client.list_tool()
+        print("Available tools:", tools)
+        if tools:
+            tool_name = tools[0].name
+            result = await client.call_tool(tool_name, "example_arg")
+            print(f"Result from {tool_name}:", result)
+        resource = await client.resource()
+        print("Resource info:", resource)   
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- server.py
+++ server.py
@@ -1,9 +1,17 @@
 from mcp.server.fastmcp import FastMCP
 
-app = FastMCP(
+mcp = FastMCP(
     name="learning-mcp",
     stateless_http=True
     
 )
+docs = {
+    "introduction": "This is a learning MCP server.",
+    "description": "This server is designed to demonstrate the capabilities of the MCP framework.",
+    "guidelines": "Follow the documentation for using the tools and resources provided by this server.",
+}
+@mcp.resource(uri="docs://documents",name="docs", description="Get documentation information",minetype="application/json")
+async def get_docs():
+    return list(docs.keys())
 
-server_app = app.streamable_http_app()
+server_app = mcp.streamable_http_app()
