7b84632e879c10d24d4b8b3f86942ee07703b560
SyedSafdarAliShah <programmersafdar@live.com>
2025-09-14 11:55:14 +0500

    updated

--- 02_learning_servers/server.py
+++ 02_learning_servers/server.py
@@ -3,12 +3,12 @@ from mcp.server.fastmcp import FastMCP
 mcp = FastMCP(
     name="learning-mcp",
     stateless_http=True
-    
+       
 )
 docs = {
     "introduction": "This is a learning MCP server.",
     "description": "This server is designed to demonstrate the capabilities of the MCP framework.",
-    "guidelines": "Follow the documentation for using the tools and resources provided by this server.",
+    "guidelines": "Follow the documentation for using the  tools and resources provided by this server.",
 }
 @mcp.resource(uri="docs://documents",name="docs", description="Get documentation information",minetype="application/json")
 async def get_docs():
